4e6126639715f907d8000dd0f087786c8ec78ac0
Roberto Mosca <robmosca@gmail.com>
2020-11-05 22:26:20 +0100

    Add serialport package

--- src/device.ts
+++ src/device.ts
@@ -1,11 +1,37 @@
-export class Device {
-  constructor(public name: string, public firmwareVersion: string) {}
+const serialPort = require("serialport");
 
-  static async pickDevice() {
-    return new Device("Test device", "0.1.0");
+export class Device {
+  public ttyDevice: string;
+  public name: string;
+  public firmwareVersion: string;
+  public serialPort: any;
+
+  constructor(ttyDevice: string) {
+    this.ttyDevice = ttyDevice;
+    this.name = ttyDevice;
+    this.firmwareVersion = "";
+    this.serialPort = undefined;
   }
 
-  public async connect() {
-    return new Promise((resolve) => setTimeout(resolve, 3000));
+  static async selectDevice() {
+    return new Device("/dev/tty.usbmodem3377397C33381");
+  }
+
+  public connect() {
+    return new Promise((resolve, reject) => {
+      this.serialPort = new serialPort(
+        this.ttyDevice,
+        {
+          baudRate: 115200,
+        },
+        function (err: Error) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
   }
 }
